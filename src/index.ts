c5daa7170cb2dd2203dfdd79c5697533e10a7c03
Mitchell Merry <mitchellmerry145@gmail.com>
2021-12-22 23:05:26 +1100

    Printing the 3x3's net works!

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,15 @@ import ThreeByThree from "./puzzles/ThreeByThree"
 
 const cube = new ThreeByThree();
 cube.print();
+
+console.log();
+cube.doNotation("U");
+cube.print();
+
+console.log();
+cube.doNotation("U2");
+cube.print();
+
+console.log();
+cube.doNotation("U'");
+cube.print();
--- src/puzzles/ThreeByThree.ts
+++ src/puzzles/ThreeByThree.ts
@@ -43,7 +43,42 @@ export default class ThreeByThree extends Puzzle {
         this.state = [...Array(6).keys()].map(i => Array.from({length: 9}).map(_ => i+1)).flat();
     }
 
+    valueMapToConsoleColour: Record<number, string> = {
+        1: "\x1b[47m", // white
+        2: "\x1b[43m", // orange (yellow)
+        3: "\x1b[102m", // green (bright green)
+        4: "\x1b[101m", // red (bright red)
+        5: "\x1b[104m", // blue (bright blue)
+        6: "\x1b[103m", // yellow (bright yellow)
+    }
+
+    // I'm just, so sorry, for what's about to happen...
     print(): void {
-        console.log(this.state)
+        const indexToChar = (index: number) => `${this.valueMapToConsoleColour[this.state[index]]}  \x1b[0m`;
+        
+        // Top face
+        for(let i = 0; i < 3; i++) {
+            console.log(`      ${indexToChar(i*3)}${indexToChar(i*3+1)}${indexToChar(i*3+1)}`)
+        }
+
+        // Middle faces
+        for(let row = 0; row < 3; row++) {
+            let toPrint = '';
+
+            for(let face = 0; face < 4; face++) {
+                for(let col = 0; col < 3; col++) {
+                    let index = 9 + row * 3 + face * 9 + col;
+                    toPrint += indexToChar(index);   
+                }
+            }
+
+            console.log(toPrint);
+        }
+
+
+        // Bottom face
+        for(let i = 0; i < 3; i++) {
+            console.log(`      ${indexToChar(5*9+i*3)}${indexToChar(5*9+i*3+1)}${indexToChar(5*9+i*3+1)}`)
+        }
     }
 }
